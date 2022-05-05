bd3f6c5266a7660fac9da53bd35ca73680dc8b78
Matheus <bonfin.santos8@gmail.com>
2022-05-05 12:52:54 -0300

    exemplos da aula 1

---
+++ type-setings.js
@@ -0,0 +1,11 @@
+const texto1 =  "Ola, mundo!";
+const texto2 =  "Ola, mundo!";
+const senha = "senhaSuperSegura";
+const strinDeNumeros = "1456653156456";
+
+const citacao = "Meu nome e ";
+const nome = "Matheus";
+
+//console.log(citacao + nome);
+
+
